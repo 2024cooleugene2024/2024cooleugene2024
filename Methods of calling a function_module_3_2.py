19b40cccc13d23835556d3eaf2ec5022fcd3d741
2024cooleugene2024 <ya@ekharchenko.ru>
2024-09-17 20:49:05 +0300

    Local to GitHub

--- Methods of calling a function_module_3_2.py
+++ Methods of calling a function_module_3_2.py
@@ -1,10 +1,11 @@
-def send_email(message, recipient, *, sender="university.help@gmail.com"):
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    def is_valid_email(email):
+        """Проверяет корректность e-mail адреса."""
+        return ("@" in email and email.endswith(('.com', '.ru', '.net')))
 
-    # Проверка корректности email (наличие @ и окончание на .com, .ru, .net)
-    valid_endings = (".com", ".ru", ".net")
-    if "@" not in recipient or not recipient.endswith(valid_endings) or "@" not in sender or not sender.endswith(
-            valid_endings):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+    # Проверка корректности email адресов
+    if not (is_valid_email(sender) and is_valid_email(recipient)):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
 
     # Проверка на отправку самому себе
@@ -14,10 +15,12 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
 
     # Проверка на отправителя по умолчанию
     if sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
     else:
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
 
+
+# Примеры использования функции
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
