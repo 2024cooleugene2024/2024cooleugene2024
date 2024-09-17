14e5aca4a176af723d110d7ed156c0249cb57973
2024cooleugene2024 <ya@ekharchenko.ru>
2024-09-17 19:36:44 +0300

    Local to Github

---
+++ Variables.py
@@ -0,0 +1,9 @@
+# Переменные
+done_tasks = 12
+spent_hours = 1.5
+course_name = 'Python'
+time_per_task = spent_hours / done_tasks
+
+# Вывод строки с использованием переменных
+print(f'Курс: {course_name}, всего задач: {done_tasks}, затрачено часов: {spent_hours},'
+      f' среднее время выполнения {time_per_task} часа.')
