09119a92d87d37c7639f13950aab1c053ec03182
2024cooleugene2024 <ya@ekharchenko.ru>
2024-09-17 19:54:30 +0300

    Local to GitHub

--- module_1_6.py
+++ module_1_6.py
@@ -1,18 +1,34 @@
-my_dict = {"Eugene": 1984}
-print(my_dict)
+# Создаем словарь с несколькими парами ключ-значение
+my_dict = {'Vasya': 1975, 'Egor': 1999, 'Masha': 2002}
+print("Initial dictionary:", my_dict)
 
-print(my_dict['Eugene'])
+# Вывод значения по существующему ключу
+print("Value for 'Masha':", my_dict['Masha'])
 
-my_dict['Kate'] = 2016
-print(my_dict)
+# Попытка получить значение по несуществующему ключу с использованием метода get()
+print("Value for 'Ivan' (non-existing):", my_dict.get('Ivan', None))
 
-my_dict.update({'Anton': 1975, 'Elena': 1981})
-print(my_dict)
+# Добавляем две новые пары ключ-значение
+my_dict['Kamila'] = 1981
+my_dict['Artem'] = 1915
+print("Dictionary after adding new pairs:", my_dict)
 
-print(my_dict)
-a = my_dict.pop('Eugene')
-print(my_dict)
-print(a)
+# Удаляем пару по ключу и выводим значение удаленного ключа
+deleted_value = my_dict.pop('Egor')
+print("Deleted value for 'Egor':", deleted_value)
 
-my_set = {1, 2, 3, 4, 5, 1, 2, 3, 4, 'string', True}
-print(my_set)
+# Выводим итоговый словарь
+print("Modified dictionary:", my_dict)
+
+# Создаем множество с повторяющимися значениями
+my_set = {1, 'Яблоко', 42.314, 'Яблоко', 1}
+print("Initial set (unique values):", my_set)
+
+# Добавляем два новых элемента в множество
+my_set.add(13)
+my_set.add((5, 6, 1.6))
+print("Set after adding new elements:", my_set)
+
+# Удаляем один элемент из множества
+my_set.remove(1)
+print("Modified set after removal:", my_set)
