c75edc168529d4adf82bc1b60b6a1cc75bee741b
2024cooleugene2024 <ya@ekharchenko.ru>
2024-11-18 18:51:35 +0300

    Local to GitHub

--- module_9_6.py
+++ module_9_6.py
@@ -1,18 +1,10 @@
 def all_variants(text):
-    """
-    Генератор возвращает подпоследовательности строки text.
-
-    :param text: Исходная строка:
-    yield: Подпоследовательность строки
-    """
-    # Внешний цикл перебирает начальные индексы
-    for start in range(len(text)):
-        # Внутренний цикл перебирает конечные индексы
-        for end in range(start + 1, len(text) + 1):
-            yield text[start:end]
+    n = len(text)
+    for length in range(1, n + 1):
+        for start in range(n - length + 1):
+            yield text[start:start + length]
 
 
-# Пример использования
 if __name__ == "__main__":
     a = all_variants("abc")
     for i in a:
